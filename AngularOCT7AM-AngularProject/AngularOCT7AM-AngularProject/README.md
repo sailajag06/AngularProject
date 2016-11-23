cd1b2b5a7b3a21d98ad3ca746d45847500364ff2
sailajag06 <sailajag06>
2016-11-24 04:08:19 +0530

    Add changes

--- AngularOCT7AM-AngularProject/AngularOCT7AM-AngularProject/README.md
+++
@@ -1,1 +0,0 @@
-# AngularOCT7AM
--- AngularOCT7AM-AngularProject/Blogger/app/app.js
+++ AngularOCT7AM-AngularProject/Blogger/app/app.js
@@ -1,17 +1,50 @@
-(function(){
+(function () {
     /*Module Creation*/
-    angular.module("BlogFace",["header"]);
-    
+    /* Integratre ui.router*/
+    angular.module("BlogFace", ["header", "ui.router", "blog"]);
+
     /*consuming the module*/
     angular.module("BlogFace")
-    .config([function(){
-       console.log("Blog Face Module");  
+        .config(["$stateProvider", "$urlRouterProvider",
+             function ($stateProvider, $urlRouterProvider) {
+                /*Blog View Integration
+                   1. Creating the state object
+                   2. registering the state object
+                   3. Invoking the state
+                */
+
+                var blogObj = {
+                    templateUrl: "app/blog/blog.html",
+                    controller: "blogCtrl",
+                    name: "Blog"
+                };
+
+                var loginObj = {
+                    templateUrl: "app/login/login.html",
+                    name: "Login"
+                };
+                var registerObj = {
+                    templateUrl: "app/register/register.html",
+                    name: "Register"
+                };
+                var homeObj = {
+                    templateUrl: "app/home/home.html",
+                    name: "Home"
+                };
+                $stateProvider.state("Blog", blogObj);
+                $stateProvider.state("Login", loginObj);
+                $stateProvider.state("Register", registerObj);
+                $stateProvider.state("Home", homeObj);
+
+
+
+                console.log("Blog Face Module");
     }])
-    .run([function(){
-        console.log("Blog Face run function");
+        .run([function () {
+            console.log("Blog Face run function");
     }]);
-    
-    
+
+
 })();
 
 /*
